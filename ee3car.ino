ffc6f6270038f7e315ab721b49b13ac2bb3bf760
Xian Kai Ng <ngxiankai286@gmail.com>
2017-11-09 14:23:53 -0800

    initial commit

--- ee3car.ino
+++ ee3car.ino
@@ -1,23 +1,5 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  
-  modified 8 Sep 2016
-  by Colby Newman
+  FOLLOW THE TRACK!
 */
 
 
@@ -87,26 +69,26 @@ void STOP(){
 
 void START(){
   analogWrite(LEFT_WHEEL, 200); //out of 255
-   analogWrite(RIGHT_WHEEL, 200);
-   Serial.print("START");
- }
+  analogWrite(RIGHT_WHEEL, 200);
+  Serial.print("START");
+}
 
 void TURN_LEFT(){
   analogWrite(LEFT_WHEEL, 40); //out of 255
-   analogWrite(RIGHT_WHEEL, 60);
+  analogWrite(RIGHT_WHEEL, 60);
   digitalWrite(BLUE_LED, HIGH);
   digitalWrite(RED_LED, LOW);
   digitalWrite(GREEN_LED, LOW);
-   
-  }
+
+}
 
 void TURN_RIGHT(){
   analogWrite(LEFT_WHEEL, 70); //out of 255
-   analogWrite(RIGHT_WHEEL, 40);
- digitalWrite(RED_LED, HIGH);
- digitalWrite(GREEN_LED, LOW);
- digitalWrite(BLUE_LED, LOW);
-  }
+  analogWrite(RIGHT_WHEEL, 40);
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
+}
 
 void GO_STRAIGHT(){
   analogWrite(RIGHT_WHEEL, 60);
@@ -114,26 +96,26 @@ void GO_STRAIGHT(){
   digitalWrite(GREEN_LED, HIGH);
   digitalWrite(RED_LED, LOW);
   digitalWrite(BLUE_LED, LOW);
-  }
+}
 
 int t = 0;
 
 // the loop function runs over and over again forever
 void loop() {
-  
-  
+
+
   left_val = (analogRead(LEFT_RECEIVER)-LEFT_BLACK)*1.0/LEFT_NORM;
   middle_val = (analogRead(MIDDLE_RECEIVER)-MIDDLE_BLACK)*1.0/MIDDLE_NORM;
   right_val = (analogRead(RIGHT_RECEIVER)-RIGHT_BLACK)*1.0/RIGHT_NORM;
   
   
-   Serial.print(left_val);
-   Serial.print(" ");
-   Serial.print(middle_val);
-   Serial.print(" ");
-   Serial.print(right_val);
-   Serial.print(" ");
-   
+  Serial.print(left_val);
+  Serial.print(" ");
+  Serial.print(middle_val);
+  Serial.print(" ");
+  Serial.print(right_val);
+  Serial.print(" ");
+
   
 
 
@@ -142,37 +124,34 @@ void loop() {
 
   int bias = (int)(40*(left_bias-right_bias));
 
-Serial.print(bias);
-Serial.println();
-if((left_val+middle_val+right_val) > 1.4)
-  if (t<20){
-    if(bias < -12) TURN_LEFT();
-    else if(bias > 8) TURN_RIGHT();
-    else  GO_STRAIGHT();
-  }
-  else {
-    t=0; 
-    START();
+  Serial.print(bias);
+  Serial.println();
+  if((left_val+middle_val+right_val) > 1.4)
+    if (t<20){
+      if(bias < -12) TURN_LEFT();
+      else if(bias > 8) TURN_RIGHT();
+      else  GO_STRAIGHT();
+    }
+    else {
+      t=0; 
+      START();
     }
     
   t++;
 
-    delay(30);
+  delay(30);
 
- /* analogWrite(LEFT_WHEEL, 60+bias); //out of 255
-  analogWrite(RIGHT_WHEEL, 60-bias); */
 
-  
 
- 
+
   if((left_val+middle_val+right_val) < 1.4){
     STOP();
   } else {
     digitalWrite(RED_LED, LOW);
-  digitalWrite(GREEN_LED, LOW);
-  digitalWrite(BLUE_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(BLUE_LED, LOW);
   }
 
- 
-   
+
+
 }
